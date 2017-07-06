0b2af1169e59cd817248ed0d6eb9b64c0b528126
Jett Durham <jett@jettdurham.com>
2017-07-06 17:03:49 -0500

    Added a few custom components, not yet clear how to externalize them and register them with CRNA + Expo...

--- TestProject1/App.js
+++ TestProject1/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+        <View>
+          <Bananas picUri="https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"/>
+        </View>
+        <View>
+          <Greeting name='Jett' />
+        </View>
       </View>
+
+    );
+  }
+}
+
+class Bananas extends Component {
+  render() {
+    let pic = {
+          uri: this.props.picUri
+    };
+
+    return (
+      <Image source={pic} style={{width: 250, height: 110}} />
+    );
+  }
+}
+
+class Greeting extends Component {
+  getName() {
+    return this.props.name
+  };
+
+  render() {
+    return (
+      <Text>Hello {this.getName()}!</Text>
     );
   }
 }
